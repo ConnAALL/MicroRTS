d671e6375315663b5ddefd4f01c3351bf6d4e0b0
asd417 <loctitematt417@gmail.com>
2025-02-17 19:40:58 -0500

    implemented simple action space

--- src/ai/AALL/JNIExpertAI.java
+++ src/ai/AALL/JNIExpertAI.java
@@ -1,8 +1,14 @@
 package ai.AALL;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import java.lang.Math;
 import ai.core.AI;
 import ai.core.AIWithComputationBudget;
@@ -30,6 +36,8 @@ public class JNIExpertAI extends AbstractionLayerAI implements JNIInterface{
     boolean firstRewardCalculation = true;
     SimpleEvaluationFunction ef = new SimpleEvaluationFunction();
     int maxAttackRadius;
+    protected HashMap<Unit, Boolean> workerTable;
+    protected HashMap<Unit, Boolean> attackerTable;
 
     public JNIExpertAI(int timeBudget, int iterationsBudget, UnitTypeTable a_utt) {
         super(new AStarPathFinding(), timeBudget, iterationsBudget);
@@ -116,15 +124,10 @@ public class JNIExpertAI extends AbstractionLayerAI implements JNIInterface{
         //System.out.println();
         return logits.length - 1; //probably floating point error...
     }
-    
-    @Override
-    public PlayerAction getAction(final int player, final GameState gs, int[][] action) throws Exception {
-        //System.out.printf("JNIExpertAI::getAction action.length %d%n", action.length);
-        //System.out.printf("JNIExpertAI::getAction action[0].length %d%n", action[0].length);
 
-        // PlayerAction pa = PlayerAction.fromVectorAction(action, gs, utt, player, maxAttackRadius);
-        // pa.fillWithNones(gs, player, 1);
-        // return pa;
+    // old model. input size 16x16 and output size 16x16+6
+    private PlayerAction getActionDetailed(final int player, final GameState gs, int[][] action)
+    {
         int[] flatAction = action[0];
         //Input a softmaxed tensor
         // output consists of 
@@ -146,24 +149,320 @@ public class JNIExpertAI extends AbstractionLayerAI implements JNIInterface{
                     int unitPos = u.getPosition(pgs);
                     int ux = unitPos / pgs.getWidth();
                     int uy = unitPos % pgs.getHeight();
-                    
+
                     //if (Math.sqrt((float) ((ux - x) ^ 2 + (uy - y) ^ 2)) < maxAttackRadius) // Check if selected tile is close enough to the unit
-                    
+
                     //System.out.printf("Unit at %d %d, acting on %d %d %n", ux, uy, x, y);
                     int ut = multinomial(softmax(unitTypeAction));
                     //System.out.printf("Evaluated Unit Type: %d %n", ut);
                     UnitType type = types.get(ut);
                     boolean shouldReduce = unitAction(player, gs, u, x, y, type);
-                    if(shouldReduce)
-                    {
+                    if (shouldReduce) {
                         flatAction[pos] *= 0.7f; //tile policy selected. Reduce it to reduce the probability of it getting reselected
                         // This is only called if the unit is movable unit
                     }
-                    
+
                 }
             }
         }
-        PlayerAction pa =  translateActions(player, gs);
+        PlayerAction pa = translateActions(player, gs);
+        pa.fillWithNones(gs, player, 1);
+        return pa;
+    }
+
+    // this function is called with all new units. All new units are by default attackers, even if they can harvest
+    private void registerNewUnit(Unit u)
+    {
+        boolean canHarvest = u.getType().canHarvest;
+        boolean canAttack = u.getType().canAttack;
+        if (attackerTable.get(u) == null && canAttack) {
+            attackerTable.put(u, true);
+        }
+        if (workerTable.get(u) == null && canHarvest) {
+            workerTable.put(u, false);
+        }
+    }
+    
+    // by default, workers can both attack and harvest. 
+    private void registerToTables(Unit u, boolean worker)
+    {
+        boolean canHarvest = u.getType().canHarvest;
+        boolean canAttack = u.getType().canAttack;
+        if (worker) {
+            if (canHarvest) {workerTable.put(u, true);}
+            if (canAttack) {attackerTable.put(u, false);}
+        } else
+        {
+            if (canHarvest) {workerTable.put(u, false);}
+            if (canAttack) {attackerTable.put(u, true);}
+        }
+    }
+
+    private void trainUnit(PhysicalGameState pgs, String UnitTypeName)
+    {
+        UnitType workerType = utt.getUnitType(UnitTypeName);
+        Collections.shuffle(pgs.getUnits()); //random unit
+        for (Unit u : pgs.getUnits()) {
+            if (u.getType().produces.contains(workerType)) {
+                train(u, workerType);
+                break;
+            }
+        }
+    }
+
+    private void attackersToQuad(int quad, PhysicalGameState pgs, List<Map.Entry<Unit, Boolean>> unitList)
+    {
+        int maxX = pgs.getWidth();
+        int maxY = pgs.getHeight();
+        switch (quad) {
+            case 1:
+                for (Map.Entry<Unit, Boolean> entry : unitList) {
+                    if (entry.getValue() == true) {
+                        int randomX = ThreadLocalRandom.current().nextInt(1, maxX / 2);
+                        int randomY = ThreadLocalRandom.current().nextInt(1, maxY / 2);
+                        move(entry.getKey(), randomX, randomY);
+                    }
+                }
+                return;
+            case 2:
+                for (Map.Entry<Unit, Boolean> entry : unitList) {
+                    if (entry.getValue() == true) {
+                        int randomX = ThreadLocalRandom.current().nextInt(maxX / 2, maxX);
+                        int randomY = ThreadLocalRandom.current().nextInt(1, maxY / 2);
+                        move(entry.getKey(), randomX, randomY);
+                    }
+                }
+                return;
+            case 3:
+                for (Map.Entry<Unit, Boolean> entry : unitList) {
+                    if (entry.getValue() == true) {
+                        int randomX = ThreadLocalRandom.current().nextInt(1, maxX / 2);
+                        int randomY = ThreadLocalRandom.current().nextInt(maxY / 2, maxY);
+                        move(entry.getKey(), randomX, randomY);
+                    }
+                }
+                return;
+            case 4:
+                for (Map.Entry<Unit, Boolean> entry : unitList) {
+                    if (entry.getValue() == true) {
+                        int randomX = ThreadLocalRandom.current().nextInt(maxX / 2, maxX);
+                        int randomY = ThreadLocalRandom.current().nextInt(maxY / 2, maxY);
+                        move(entry.getKey(), randomX, randomY);
+                    }
+                }
+                return;
+        }
+    }
+
+    private boolean resourceInRange(PhysicalGameState pgs, int x, int y, int range)
+    {
+        Collection<Unit> units = pgs.getUnitsAround(x, y, range);
+        for (Unit u : units) {
+            if (u.getType().isResource)
+                return true;
+        }
+        return false;
+    }
+    
+    private int[] findBarrackLocation(PhysicalGameState pgs, int range) {
+        try{
+            for (Unit u : pgs.getUnits()) {
+                if(u.getType().isStockpile)
+                {
+                    int ux = u.getX();
+                    int uy = u.getY();
+                    for (int i = Math.max(0, ux - range); i < Math.min(pgs.getWidth()-1, ux + range); i++){
+                        for (int j = Math.max(0, uy - range); j < Math.min(pgs.getHeight()-1, uy + range); j++){
+                            if (pgs.getTerrain(i, j) == 0 && pgs.getUnitAt(i, j) == null && !resourceInRange(pgs,i,j,3)) {
+                                return new int[] { i, j };
+                            }
+                        }
+                    }   
+                }
+            }
+            return null; // No valid expansion found
+        } catch (IndexOutOfBoundsException e)
+        {
+            System.out.println("Index out of range while searching for barrack building position");
+        }
+        return null;
+    }
+
+    private int[] findExpansionLocation(PhysicalGameState pgs, int range) {
+        try {
+            for (Unit u : pgs.getUnits()) {
+                if (u.getType().isStockpile) {
+                    int ux = u.getX();
+                    int uy = u.getY();
+                    for (int i = Math.max(0, ux - range); i < Math.min(pgs.getWidth() - 1, ux + range); i++) {
+                        for (int j = Math.max(0, uy - range); j < Math.min(pgs.getHeight() - 1, uy + range); j++) {
+                            if (pgs.getTerrain(i, j) == 0 && pgs.getUnitAt(i, j) == null
+                                    && resourceInRange(pgs, i, j, 3)) {
+                                return new int[] { i, j };
+                            }
+                        }
+                    }
+                }
+            }
+            return null; // No valid expansion found
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Index out of range while searching for expansion");
+        }
+        return null;
+    }
+    
+    private boolean isBuilding(Unit u, GameState gs)
+    {
+        return gs.getActionAssignment(u).action.getType() == UnitAction.TYPE_PRODUCE;
+    }
+
+    @Override
+    public PlayerAction getAction(final int player, final GameState gs, int[][] action) throws Exception {
+        int[] flatAction = action[0];
+        assert flatAction.length == 12 : "Model action vector does not match action count";
+        int agentAction = multinomial(softmax(flatAction));
+        PhysicalGameState pgs = gs.getPhysicalGameState();
+        for (Unit u : pgs.getUnits()) {
+            if (u.getPlayer() == player)
+            {
+                registerNewUnit(u);
+            }
+        }
+        List<Map.Entry<Unit, Boolean>> unitList;
+        
+        int[] coords;
+        
+        // These actions override previous action unless it's a building action
+        switch (agentAction) {
+            case 0: // do nothing
+                break;
+            case 1: // allocate 1 worker unit to resource gathering. The worker set to resource gathering does not take attack orders
+                unitList = new ArrayList<>(workerTable.entrySet());
+                Collections.shuffle(unitList); //random unit
+                for (Map.Entry<Unit, Boolean> entry : unitList) {
+                    Unit u = entry.getKey();
+                    if (entry.getValue() == false) {
+                        registerToTables(entry.getKey(), true);
+                        break;
+                    }
+                }
+                break; // no units can be set as worker
+            case 2: // deallocate all workers from resource gathering. (Worker pull)
+                for (Unit entry : workerTable.keySet()) {
+                    registerToTables(entry, false);
+                }
+                break;
+            // attack commands sends all units that arent resource gathers to a random position within the specified quadrant
+            case 3: // attack quadrant 1 -> on default map this is the base territory
+                unitList = new ArrayList<>(attackerTable.entrySet());
+                attackersToQuad(1, pgs, unitList);
+                break;
+            case 4: // attack quadrant 2 
+                unitList = new ArrayList<>(attackerTable.entrySet());
+                attackersToQuad(2, pgs, unitList);
+                break;
+            case 5: // attack quadrant 3
+                unitList = new ArrayList<>(attackerTable.entrySet());
+                attackersToQuad(3, pgs, unitList);
+                break;
+            case 6: // attack quadrant 4 -> on default map this is the enemy base
+                unitList = new ArrayList<>(attackerTable.entrySet());
+                attackersToQuad(4, pgs, unitList);
+                break;
+            case 7: // build worker from random base
+                trainUnit(pgs, "Worker");
+                break;
+            case 8: // build light from random barrack
+                trainUnit(pgs, "Light");
+                break;
+            case 9: // build heavy from random barrack
+                trainUnit(pgs, "Heavy");
+                break;
+            case 10: // build ranged from random barrack
+                trainUnit(pgs, "Ranged");
+                break;
+            case 11: // expand to nearest base that is (1. further than 5 tiles from current base) (2. has resource within 3 tiles)
+                coords = findExpansionLocation(pgs, 5);
+                if (coords != null)
+                {
+                    for(Unit u : workerTable.keySet())
+                    {
+                        UnitType ut = utt.getUnitType("Base");
+                        if(u.getType().produces.contains(ut) && !isBuilding(u,gs)) // make sure the unit is not already building something
+                        {
+                            build(u, ut, coords[0], coords[1]);
+                        }
+                    }
+                }
+                break;
+            case 12: // build barrack near a random base (1. within 5 tiles of an existing base) (2. at least 3 tiles away from existing resources)
+                coords = findBarrackLocation(pgs, 5);
+                if (coords != null)
+                {
+                    for(Unit u : workerTable.keySet())
+                    {
+                        UnitType ut = utt.getUnitType("Barracks");
+                        if(u.getType().produces.contains(ut) && !isBuilding(u,gs)) // make sure the unit is not already building something
+                        {
+                            build(u, ut, coords[0], coords[1]); 
+                        }
+                    }
+                }
+                break;
+        }
+        
+        //Auto Actions
+        //Attackers auto attacking around them
+        for(Unit u : attackerTable.keySet())
+        {
+            Boolean ab = workerTable.get(u);
+            if(ab != null && ab && gs.getActionAssignment(u) == null)
+            {
+                int minRange = Math.max(2, u.getAttackRange());
+                Collection<Unit> inRange = pgs.getUnitsAround(u.getX(), u.getY(), minRange);
+                for (Unit uu : inRange) {
+                    if (uu.getPlayer() != player) {
+                        attack(u, uu);
+                    }
+                }
+            }
+        }
+        //Workers auto harvest from around them.
+        for(Unit u : workerTable.keySet())
+        {
+            Boolean wb = workerTable.get(u);
+            if(wb != null && wb && gs.getActionAssignment(u) == null)
+            {
+                int minRange = Math.max(4,u.getAttackRange());
+                Collection<Unit> inRange = pgs.getUnitsAround(u.getX(), u.getY(), minRange);
+                Unit base = null;
+                for(Unit uu : inRange) // find close base
+                {
+                    if (uu.getPlayer() == player && uu.getType().isStockpile)
+                    {
+                        base = uu;
+                    }
+                }
+                if (base == null) //find any base
+                {
+                    for (Unit b : pgs.getUnits()) {
+                        if(b.getType().isStockpile)
+                        {
+                            move(u, b.getX(), b.getY()); 
+                        }
+                    }
+                }
+                for(Unit uuu : inRange)
+                {
+                    if(uuu.getType().isResource)
+                    {
+                        harvest(u, uuu, base);
+                    }
+                }
+            }
+        }
+        
+        PlayerAction pa = translateActions(player, gs);
         pa.fillWithNones(gs, player, 1);
         return pa;
     }
@@ -250,6 +549,7 @@ public class JNIExpertAI extends AbstractionLayerAI implements JNIInterface{
     @Override
     public void reset() {
         // TODO Auto-generated method stub
+        workerTable = new HashMap<Unit, Boolean>(30);
     }
 
     @Override
