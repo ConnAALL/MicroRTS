34d0c066a3b7ff6bee887e0f12ed8407ea0a888c
asd417 <loctitematt417@gmail.com>
2025-02-18 16:34:11 -0500

    added error checkings

--- src/ai/AALL/JNIExpertAI.java
+++ src/ai/AALL/JNIExpertAI.java
@@ -174,150 +174,165 @@ public class JNIExpertAI extends AbstractionLayerAI implements JNIInterface{
 
     private PlayerAction getActionSimple(final int player, final GameState gs, int[][] action)
     {
-        int[] flatAction = action[0];
-        assert flatAction.length == 12 : "Model action vector does not match action count";
-        int agentAction = multinomial(softmax(flatAction));
+        System.out.println("Calculating Simple Action");
         PhysicalGameState pgs = gs.getPhysicalGameState();
+        int[] flatAction = null;
+        int agentAction = 0;
+        List<Map.Entry<Unit, Boolean>> unitList;
+        int[] coords;
         for (Unit u : pgs.getUnits()) {
             if (u.getPlayer() == player)
             {
                 registerNewUnit(u);
             }
         }
-        List<Map.Entry<Unit, Boolean>> unitList;
-        
-        int[] coords;
-        
-        // These actions override previous action unless it's a building action
-        switch (agentAction) {
-            case 0: // do nothing
-                break;
-            case 1: // allocate 1 worker unit to resource gathering. The worker set to resource gathering does not take attack orders
-                unitList = new ArrayList<>(workerTable.entrySet());
-                Collections.shuffle(unitList); //random unit
-                for (Map.Entry<Unit, Boolean> entry : unitList) {
-                    Unit u = entry.getKey();
-                    if (entry.getValue() == false) {
-                        registerToTables(entry.getKey(), true);
-                        break;
-                    }
-                }
-                break; // no units can be set as worker
-            case 2: // deallocate all workers from resource gathering. (Worker pull)
-                for (Unit entry : workerTable.keySet()) {
-                    registerToTables(entry, false);
-                }
-                break;
-            // attack commands sends all units that arent resource gathers to a random position within the specified quadrant
-            case 3: // attack quadrant 1 -> on default map this is the base territory
-                unitList = new ArrayList<>(attackerTable.entrySet());
-                attackersToQuad(1, pgs, unitList);
-                break;
-            case 4: // attack quadrant 2 
-                unitList = new ArrayList<>(attackerTable.entrySet());
-                attackersToQuad(2, pgs, unitList);
-                break;
-            case 5: // attack quadrant 3
-                unitList = new ArrayList<>(attackerTable.entrySet());
-                attackersToQuad(3, pgs, unitList);
-                break;
-            case 6: // attack quadrant 4 -> on default map this is the enemy base
-                unitList = new ArrayList<>(attackerTable.entrySet());
-                attackersToQuad(4, pgs, unitList);
-                break;
-            case 7: // build worker from random base
-                trainUnit(pgs, "Worker");
-                break;
-            case 8: // build light from random barrack
-                trainUnit(pgs, "Light");
-                break;
-            case 9: // build heavy from random barrack
-                trainUnit(pgs, "Heavy");
-                break;
-            case 10: // build ranged from random barrack
-                trainUnit(pgs, "Ranged");
-                break;
-            case 11: // expand to nearest base that is (1. further than 5 tiles from current base) (2. has resource within 3 tiles)
-                coords = findExpansionLocation(pgs, 5);
-                if (coords != null)
-                {
-                    for(Unit u : workerTable.keySet())
-                    {
-                        UnitType ut = utt.getUnitType("Base");
-                        if(u.getType().produces.contains(ut) && !isBuilding(u,gs)) // make sure the unit is not already building something
-                        {
-                            build(u, ut, coords[0], coords[1]);
-                        }
-                    }
-                }
-                break;
-            case 12: // build barrack near a random base (1. within 5 tiles of an existing base) (2. at least 3 tiles away from existing resources)
-                coords = findBarrackLocation(pgs, 5);
-                if (coords != null)
-                {
-                    for(Unit u : workerTable.keySet())
-                    {
-                        UnitType ut = utt.getUnitType("Barracks");
-                        if(u.getType().produces.contains(ut) && !isBuilding(u,gs)) // make sure the unit is not already building something
-                        {
-                            build(u, ut, coords[0], coords[1]); 
-                        }
-                    }
-                }
-                break;
-        }
-        
-        //Auto Actions
-        //Attackers auto attacking around them
-        for(Unit u : attackerTable.keySet())
+        try {
+            flatAction = action[0];
+            assert action.length == 1 : "Model action vector height must be 1";
+            assert flatAction.length == 12 : "Model action vector does not match action count";
+            agentAction = multinomial(softmax(flatAction));
+        } catch(Exception e)
         {
-            Boolean ab = workerTable.get(u);
-            if(ab != null && ab && gs.getActionAssignment(u) == null)
+            System.out.println("Error while parsing model output: " + e.getMessage());
+        }
+        try{
+            // These actions override previous action unless it's a building action
+            switch (agentAction) {
+                case 0: // do nothing
+                    break;
+                case 1: // allocate 1 worker unit to resource gathering. The worker set to resource gathering does not take attack orders
+                    unitList = new ArrayList<>(workerTable.entrySet());
+                    Collections.shuffle(unitList); //random unit
+                    for (Map.Entry<Unit, Boolean> entry : unitList) {
+                        Unit u = entry.getKey();
+                        if (entry.getValue() == false) {
+                            registerToTables(entry.getKey(), true);
+                            break;
+                        }
+                    }
+                    break; // no units can be set as worker
+                case 2: // deallocate all workers from resource gathering. (Worker pull)
+                    for (Unit entry : workerTable.keySet()) {
+                        registerToTables(entry, false);
+                    }
+                    break;
+                // attack commands sends all units that arent resource gathers to a random position within the specified quadrant
+                case 3: // attack quadrant 1 -> on default map this is the base territory
+                    unitList = new ArrayList<>(attackerTable.entrySet());
+                    attackersToQuad(1, pgs, unitList);
+                    break;
+                case 4: // attack quadrant 2 
+                    unitList = new ArrayList<>(attackerTable.entrySet());
+                    attackersToQuad(2, pgs, unitList);
+                    break;
+                case 5: // attack quadrant 3
+                    unitList = new ArrayList<>(attackerTable.entrySet());
+                    attackersToQuad(3, pgs, unitList);
+                    break;
+                case 6: // attack quadrant 4 -> on default map this is the enemy base
+                    unitList = new ArrayList<>(attackerTable.entrySet());
+                    attackersToQuad(4, pgs, unitList);
+                    break;
+                case 7: // build worker from random base
+                    trainUnit(pgs, "Worker");
+                    break;
+                case 8: // build light from random barrack
+                    trainUnit(pgs, "Light");
+                    break;
+                case 9: // build heavy from random barrack
+                    trainUnit(pgs, "Heavy");
+                    break;
+                case 10: // build ranged from random barrack
+                    trainUnit(pgs, "Ranged");
+                    break;
+                case 11: // expand to nearest base that is (1. further than 5 tiles from current base) (2. has resource within 3 tiles)
+                    coords = findExpansionLocation(pgs, 5);
+                    if (coords != null)
+                    {
+                        for(Unit u : workerTable.keySet())
+                        {
+                            UnitType ut = utt.getUnitType("Base");
+                            if(u.getType().produces.contains(ut) && !isBuilding(u,gs)) // make sure the unit is not already building something
+                            {
+                                build(u, ut, coords[0], coords[1]);
+                            }
+                        }
+                    }
+                    break;
+                case 12: // build barrack near a random base (1. within 5 tiles of an existing base) (2. at least 3 tiles away from existing resources)
+                    coords = findBarrackLocation(pgs, 5);
+                    if (coords != null)
+                    {
+                        for(Unit u : workerTable.keySet())
+                        {
+                            UnitType ut = utt.getUnitType("Barracks");
+                            if(u.getType().produces.contains(ut) && !isBuilding(u,gs)) // make sure the unit is not already building something
+                            {
+                                build(u, ut, coords[0], coords[1]); 
+                            }
+                        }
+                    }
+                    break;
+            }
+        } catch(Exception e)
+        {
+            System.out.println("Error while parsing model output: " + e.getMessage());
+        }
+        try{
+            //Auto Actions
+            //Attackers auto attacking around them
+            for(Unit u : attackerTable.keySet())
             {
-                int minRange = Math.max(2, u.getAttackRange());
-                Collection<Unit> inRange = pgs.getUnitsAround(u.getX(), u.getY(), minRange);
-                for (Unit uu : inRange) {
-                    if (uu.getPlayer() != player) {
-                        attack(u, uu);
+                Boolean ab = workerTable.get(u);
+                if(ab != null && ab && gs.getActionAssignment(u) == null)
+                {
+                    int minRange = Math.max(2, u.getAttackRange());
+                    Collection<Unit> inRange = pgs.getUnitsAround(u.getX(), u.getY(), minRange);
+                    for (Unit uu : inRange) {
+                        if (uu.getPlayer() != player) {
+                            attack(u, uu);
+                        }
                     }
                 }
             }
-        }
-        //Workers auto harvest from around them.
-        for(Unit u : workerTable.keySet())
-        {
-            Boolean wb = workerTable.get(u);
-            if(wb != null && wb && gs.getActionAssignment(u) == null)
+            //Workers auto harvest from around them.
+            for(Unit u : workerTable.keySet())
             {
-                int minRange = Math.max(4,u.getAttackRange());
-                Collection<Unit> inRange = pgs.getUnitsAround(u.getX(), u.getY(), minRange);
-                Unit base = null;
-                for(Unit uu : inRange) // find close base
+                Boolean wb = workerTable.get(u);
+                if(wb != null && wb && gs.getActionAssignment(u) == null)
                 {
-                    if (uu.getPlayer() == player && uu.getType().isStockpile)
+                    int minRange = Math.max(4,u.getAttackRange());
+                    Collection<Unit> inRange = pgs.getUnitsAround(u.getX(), u.getY(), minRange);
+                    Unit base = null;
+                    for(Unit uu : inRange) // find close base
                     {
-                        base = uu;
-                    }
-                }
-                if (base == null) //find any base
-                {
-                    for (Unit b : pgs.getUnits()) {
-                        if(b.getType().isStockpile)
+                        if (uu.getPlayer() == player && uu.getType().isStockpile)
                         {
-                            move(u, b.getX(), b.getY()); 
+                            base = uu;
+                        }
+                    }
+                    if (base == null) //find any base
+                    {
+                        for (Unit b : pgs.getUnits()) {
+                            if(b.getType().isStockpile)
+                            {
+                                move(u, b.getX(), b.getY()); 
+                            }
+                        }
+                    }
+                    for(Unit uuu : inRange)
+                    {
+                        if(uuu.getType().isResource)
+                        {
+                            harvest(u, uuu, base);
                         }
                     }
                 }
-                for(Unit uuu : inRange)
-                {
-                    if(uuu.getType().isResource)
-                    {
-                        harvest(u, uuu, base);
-                    }
-                }
             }
+        } catch(Exception e)
+        {
+            System.out.println("Error while handling automatic commands: " + e.getMessage());
         }
-        
         PlayerAction pa = translateActions(player, gs);
         pa.fillWithNones(gs, player, 1);
         return pa;
